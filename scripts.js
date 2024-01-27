18a6ff83a0e60d1ff2be426d36d7aa6e91e1bde3
Ruslan <ruslanilasov5@gmail.com>
2024-01-28 05:19:54 +0700

    added score

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 const elements = document.querySelector('.grid__line'); 
 const box = document.querySelectorAll('.grid__element'); 
+let heading = document.getElementById('red_score');
+
+let red; let white;
 let win;
-let flag = 0, rowCount = 0, colCount = 0, ldCount = 0, rdCount = 0;
+let flag = 1, rowCount = 0, colCount = 0, ldCount = 0, rdCount = 0;
 let arr = [[4,3,0],[0,7,5],[0,3,0]]; 
 let losLeftCount, losRightCount, losCol, losRow;
+let scoreRed = 0;
+let scoreWhite = 0;
+let animatedScoreRed;
+let animatedScoreWhite;
  
-// По клику 
 elements.addEventListener('click', function(event){ 
     if (event.target.classList.contains('grid__element')){ 
         console.log('Flag: ', flag); 
@@ -22,10 +28,9 @@ elements.addEventListener('click', function(event){
         console.log(`Координаты ${x}, ${y}`); 
         arr[x][y] = (flag % 2) + 1; 
         console.log('Цвет ',arr[x][y]); 
-        // Вызвать функцию которая будет проверять победу перебор данных массива  
     }; 
  
-    if(flag > 4){ 
+    if(flag > 5){ 
         checkRow(); 
         checkColumn();
         checkDiagonals();
@@ -34,7 +39,7 @@ elements.addEventListener('click', function(event){
 
 }); 
  
- 
+ ``
 function checkRow(){ 
     // console.log(arr); 
     console.log('FinishString'); 
@@ -43,6 +48,11 @@ function checkRow(){
             if(arr[i][j] == arr[i][j+1] && arr[i][j] == arr[i][j+2]){
                 console.log('Строка равна ПОБЕДА');
                 win = true;
+                if (arr[i][j] == 1){
+                    redHTML();
+                }else{
+                   whiteHTML();
+                };
             }
             else{
                 console.log('Строка разная');
@@ -55,9 +65,10 @@ function checkRow(){
 function checkColumn(){
     for(let j = 0; j < 3; j++){ 
         for (let i = 0; i < 1; i++){ 
-            if(arr[i][j] == arr[i+1][j] && arr[i+2][j] == arr[i][j+2]){
+            if(arr[i][j] == arr[i+1][j] && arr[i][j] == arr[i+2][j]){   
                 console.log('Столбец одинаквый ПОБЕДА');
                 win = true;
+                whoWin();
             }
             else{
                 console.log('Столбец разный');
@@ -73,6 +84,7 @@ function checkDiagonals(){
             if(arr[i][j] == arr[i+1][j+1] && arr[i][j] == arr[i+2][j+2]){
                 console.log('Левая диагональ равна ПОБЕДА');
                 win = true;
+                whoWin();
             }
             else{
                 console.log('Левая диагональ не равна');
@@ -84,6 +96,7 @@ function checkDiagonals(){
             if(arr[i][j] == arr[i+1][j-1] && arr[i][j] == arr[i+2][j-2]){
                 console.log('Правая диагональ равна ПОБЕДА');
                 win = true;
+                whoWin();
             }
             else{
                 console.log('Правая диагональ не равна');
@@ -94,7 +107,6 @@ function checkDiagonals(){
     // if(ldCount == 1 ){losLeftCount = ldCount};
     // if(rdCount == 1 ){losRightCount = rdCount};
 };
-
 
 function animatedFinish(){
     if(win == true){
@@ -111,3 +123,22 @@ function animatedFinish(){
     //     };
     // };
 };
+
+function score(){
+
+};
+
+function redHTML(){
+    red = true;
+    scoreRed++;
+    heading.insertAdjacentHTML('beforeEnd', '');
+    animatedScoreRed = `<h1 id="score-red">${scoreRed}</h1>`;
+    heading.insertAdjacentHTML('afterBegin', animatedScoreRed);
+}
+function whiteHTML(){
+    white = true;
+    scoreWhite++;
+    const clear = '';
+    animatedScoreWhite = `<h1 id="score-white">${scoreWhite}</h1>`;
+    heading.insertAdjacentHTML('beforeEnd', animatedScoreWhite);
+}
